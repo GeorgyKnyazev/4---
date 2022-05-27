f6d455e1bedc81a2810bda329f39d22e6313470f
Георгий Князев <saint_georgy@yahoo.com>
2022-05-27 12:07:05 +0300

    Добавьте файлы проекта.

---
+++ 4 - задание/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4___задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPicturesInRow = 3;
+            int imagesFromUser = 52;
+            int fullyFilledRows = imagesFromUser / numberOfPicturesInRow;
+            int picturesInAnIncompleteRow = imagesFromUser % numberOfPicturesInRow;
+            Console.WriteLine("Количество заполненых рядов " + fullyFilledRows + ", кртинок в не полном ряду " + picturesInAnIncompleteRow + ".");
+        }
+    }
+}
